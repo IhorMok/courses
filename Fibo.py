611d28949a0f2b711ef16eca82a55b90456b9e2a
IhorMok <47272869+IhorMok@users.noreply.github.com>
2019-05-02 17:47:03 +0300

    Add files via upload

---
+++ Fibo.py
@@ -0,0 +1,8 @@
+def fibo(n):
+    f = [0, 1]
+    for i in range(2, n + 1):
+        f.append(f[i-1] + f[i-2])
+    print(f)
+
+n = 10
+fibo(n)
---
+++ Whileee.py
@@ -0,0 +1,4 @@
+i=-10
+while i<=32:
+    print(i,'Good')
+    i+=3
